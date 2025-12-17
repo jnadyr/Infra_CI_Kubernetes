00a27b6e2b3fd9e5c36a7d61aeac1a218fdc7905
José Nadyr <112030141+jnadyr@users.noreply.github.com>
2025-12-17 17:03:14 -0300

    Update EKS.tf

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -1,11 +1,9 @@
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-#  version = ">= 4.47.0" # Adicione ou atualize para uma versão recente # adicionado com ajuda da IA por JNOD
-
-#  cluster_name                    = var.cluster_name
-#  cluster_version                 = "1.24"
-#  cluster_endpoint_public_access  = true
+  cluster_name                    = var.cluster_name
+  cluster_version                 = "1.24"
+  cluster_endpoint_public_access  = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
